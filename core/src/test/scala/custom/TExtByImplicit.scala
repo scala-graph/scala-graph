88040ce0fcf9cf1f7e6d1e82ebf42579d91bc67f
Peter Empen <postmaster@scala-graph.org>
2013-06-09 10:29:48 +0200

    fix/more documentation

--- core/src/test/scala/custom/TExtByImplicit.scala
+++ core/src/test/scala/custom/TExtByImplicit.scala
@@ -15,90 +15,100 @@ class TExtByImplicitTest
 	with	  ShouldMatchers
 {
   def test_graphEnrichment {
-    /*
-     * Enriches Graph with custom methods.
+    /* Provide graph enrichment.
      */
-    final class ExtGraph[N, E[X] <: EdgeLikeIn[X]](val g: Graph[N,E]) {
-      /*
-       * Set of all directed edges contained - makes sense for mixed graphs only.
+    implicit final class ExtGraph[N, E[X] <: EdgeLikeIn[X]](val g: Graph[N,E]) {
+      
+      /* Set of all directed edges contained in g.
        */
       def diEdges = g.edges filter (_.directed)
     }
-    implicit def gToExtG[N, E[X] <: EdgeLikeIn[X]](g: Graph[N,E]) =
-      new ExtGraph[N,E](g)
-
-    // test enrichment --------------
+    /* Consume graph enrichment.
+     */
     Graph(1~2,  2~3).diEdges should be ('isEmpty)
     Graph(1~2, 2~>3).diEdges should have size (1)
   }
   
-  def test_enrichedUnDiGraph {
-    /*
-     * Restrict enrichment to undirected graphs only.
+  def test_unDiGraphEnrichment {
+    /* Provide graph enrichment restricted to undirected graphs.
+     * Note that a lower bound must also be defined to exclude directed edges.
      */
-    final class UnDiGraph[N, E[X] >: UnDiEdge[X] <: UnDiEdge[X]](val g: Graph[N,E]) {
+    implicit final class UnDiGraph[N, E[X] >: UnDiEdge[X] <: UnDiEdge[X]]
+        (val g: Graph[N,E]) {
       def alwaysTrue = true
     }
-    implicit def gToUnDiG[N, E[X] >: UnDiEdge[X] <: UnDiEdge[X]](g: Graph[N,E]) =
-      new UnDiGraph[N,E](g)
-    
+    /* Consume enrichment.  
+     */
     Graph(1~2).alwaysTrue should be (true)
-    // must not compile:
+    /* Comment in the following lines to assure of getting compiler errors
+     * when using other then undirected graphs.
+     */
     // Graph(1~2~3).alwaysTrue 
     // Graph(1~>2).alwaysTrue
   }
   
   def test_nodeEnrichment_1 {
-    // provide enrichment --------------
-    final class ExtGraphNode[N, E[X] <: EdgeLikeIn[X]](node_ : Graph[N,E]#NodeT)
-    {
+    /* Provide node enrichment.
+     * Note that this way of enrichment is not suitable for methods returning
+     * inner nodes or inner edges.
+     */
+    implicit final class ExtGraphNode[N, E[X] <: EdgeLikeIn[X]]
+        (node_ : Graph[N,E]#NodeT) {
       type NodeT = graph.NodeT
       val graph = node_.containingGraph
       val node  = node_.asInstanceOf[NodeT]
-      /*
-       * Calculates the sum of weights of outgoing edges from this node.
+
+      /* Calculates the sum of weights of outgoing edges from this node.
        */
       def outgoingWeights = node.outgoing.map(_.weight).sum
     }
-    implicit def nodeToExtN[N, E[X] <: EdgeLikeIn[X]] (node: Graph[N,E]#NodeT) =
-      new ExtGraphNode[N,E](node)
-
-    // consume enrichment --------------
+    /* Consume node enrichment.
+     */
     import scalax.collection.edge.Implicits._
 
-    implicit val g = Graph((1~%2)(1),  (1~%3)(2), (2~%3)(3))
+    val g = Graph((1~%2)(1),  (1~%3)(2), (2~%3)(3))
     (g get 1).outgoingWeights should be (3)
     (g get 2).outgoingWeights should be (4)
     (g get 3).outgoingWeights should be (5)
-    
   }
   
   def test_nodeEnrichment_2 {
-    // provide enrichment --------------
+    /* Provide node enrichment also suitable for methods returning inner nodes.
+     * Note that compared with test_nodeEnrichment_3, `require` and `new Object`
+     * lead to higher resource consumption. 
+     */
     implicit def toExtNode[N, E[X] <: EdgeLikeIn[X]]
         (node: Graph[N,E]#NodeT)(implicit graph: Graph[N,E]) = {
       require(node.containingGraph eq graph,
           "'node' must be an inner node of the implicit 'graph' instance.")
       new Object {
-        def inHead = node.inNeighbors.head.asInstanceOf[graph.NodeT]
+        def inHeadOption = node.inNeighbors.headOption.
+            asInstanceOf[Option[graph.NodeT]]
       }
     }
-    // consume enrichment --------------
-    implicit val g = Graph(1 ~> 2, 2 ~> 3)
-    (g get 1).inHead.shortestPathTo(g get 3) should be ('isDefined)
+    /* Consume node enrichment.
+     */
+    implicit val g = Graph(1 ~> 2, 2 ~> 3, 2 ~> 1)
+    (g get 1).inHeadOption.map(_.shortestPathTo(g get 3)) should be ('isDefined)
   }
 
   def test_nodeEnrichment_3 {
-    // provide enrichment --------------
-    class ExtGraph[N, E[X] <: EdgeLikeIn[X], G <: Graph[N,E]](val graph: G) {
+    /* Provide node enrichment also suitable for methods returning inner nodes.
+     * This is the recommended, type safe way of extending nodes. 
+     */
+    final class ExtGraph[N, E[X] <: EdgeLikeIn[X], G <: Graph[N,E]](val graph: G) {
       implicit final class ExtNode(node: graph.type#NodeT) {
-        def inHead = node.inNeighbors.head
+        def inHeadOption = node.inNeighbors.headOption
       }
     }
-    // consume enrichment --------------
-    val g = Graph(1 ~> 2, 2 ~> 3)
-    val e = new ExtGraph[Int,DiEdge,g.type](g)
-    import e.ExtNode
-    (g get 1).inHead.shortestPathTo(g get 3) should be ('isDefined)
+    /* Consume node enrichment.
+     * You need to instantiate `ExtGraph` for each graph separately and import
+     * the contained implicit conversion.
+     */
+    val g = Graph(1 ~> 2, 2 ~> 3, 2 ~> 1)
+    val enriched = new ExtGraph[Int,DiEdge,g.type](g)
+    import enriched.ExtNode
+
+    (g get 1).inHeadOption.map(_.shortestPathTo(g get 3)) should be ('isDefined)
   }
 }
