fcda4f0bb719ced0f04d71dff46ba821ab923469
Christopher Métrailler <christopher.metrailler@epfl.ch>
2015-01-02 21:34:17 +0100

    mod: type safe support for DOT ID and DOT node_id that were covered by String previously. new: DOT record support.

--- project/Version.scala
+++ project/Version.scala
@@ -3,14 +3,14 @@ object Version {
   val compiler_2 = "2.10.3"
   val compilerIsRC = false
 
-  private val isSnapshot = true
+  private val isSnapshot = false
   private def snapshot = if (isSnapshot) "-SNAPSHOT" else ""
   private val major = "1.9"
-  val all         = major + ".0" + snapshot
-  val core        = major + ".1" + snapshot
-  val constrained = major + ".0" + snapshot
-  val dot         = major + ".0" + snapshot
-  val json        = major + ".2" + snapshot
-  val test        = major + ".0" + snapshot
-  val misc        = major + ".0" + snapshot
+  val all         = s"$major.0$snapshot"
+  val core        = s"$major.1$snapshot"
+  val constrained = s"$major.0$snapshot"
+  val dot         =   s"1.10.0$snapshot"
+  val json        = s"$major.2$snapshot"
+  val test        = s"$major.0$snapshot"
+  val misc        = s"$major.0$snapshot"
 }
