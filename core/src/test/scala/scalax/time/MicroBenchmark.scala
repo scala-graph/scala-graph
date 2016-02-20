3f551b0e6b551d26630c2b7c434bd4d530dbeb60
Peter Empen <postmaster@scala-graph.org>
2016-02-20 12:56:40 +0100

    improved time measurement

--- core/src/test/scala/scalax/time/MicroBenchmark.scala
+++ core/src/test/scala/scalax/time/MicroBenchmark.scala
@@ -1,111 +1,129 @@
 package scalax.time
 
 import scala.language.implicitConversions
+import scala.collection.AbstractTraversable
+import scala.collection.mutable.ArrayBuffer
 
 /** Provides lightweight syntax for simple time measurement and the comparison of results.
- * Not aimed at sophisticated JVM benchmarking.
- */
+  * Not aimed at sophisticated JVM benchmarking.
+  *
+  * Use `time`, `measure` or `measureAll` to get absolute times.
+  * Use `relativeTime` or `relativeTimes` to get relative times.
+  * Except `time`, all methods return `Result` that is comprised of the computation result(s) and elapsed times.
+  * `warmUp` is the number of repeated computations prior to any measurement.
+  * `repetitions` is the number of the repeated computation that will be measured.
+  */
 object MicroBenchmark {
   import scala.math.BigInt._
   import scala.math.Numeric
 
-  final case class NanoSecond(val self: Long) {
-    override def toString = self.toString + " ns" 
+  final implicit class NanoSecond(val value: Long) extends AnyVal {
     def relativeTo(decimals: Int = 2)(that: NanoSecond): Float =
-      round(this.self.toFloat / that.self.toFloat, decimals)
+      round(this.value.toFloat / that.value.toFloat, decimals)
   }
-  implicit def longToNanoSecond(ns: Long): NanoSecond = NanoSecond(ns)
-  implicit def nanoSecondToLong(ns: NanoSecond): Long = ns.self
-  
-  // allows to call functions requiring implicit Numeric such as sum 
+
+  // allows to call functions requiring implicit Numeric such as sum
   implicit object NanoSecondNumeric extends Numeric[NanoSecond] {
+    implicit def nanoSecondToLong(ns: NanoSecond): Long = ns.value
     val num = Numeric.LongIsIntegral
     def plus    (x: NanoSecond, y: NanoSecond) = num.plus(x,y)
     def minus   (x: NanoSecond, y: NanoSecond) = num.minus(x,y)
     def times   (x: NanoSecond, y: NanoSecond) = num.times(x,y)
     def negate  (x: NanoSecond) = num.negate(x)
-    def fromInt (x: Int) = x
-    def toInt   (x: NanoSecond) = x.self.toInt
-    def toLong  (x: NanoSecond) = x.self.toLong
-    def toFloat (x: NanoSecond) = x.self.toFloat
-    def toDouble(x: NanoSecond) = x.self.toDouble
-    def compare (x:NanoSecond, y:NanoSecond) = num.compare(x,y)
+    def fromInt (x: Int) = x.toLong
+    def toInt   (x: NanoSecond) = x.value.toInt
+    def toLong  (x: NanoSecond) = x.value.toLong
+    def toFloat (x: NanoSecond) = x.value.toFloat
+    def toDouble(x: NanoSecond) = x.value.toDouble
+    def compare (x: NanoSecond, y: NanoSecond) = num.compare(x,y)
   }
 
-  case class Result[A](nanoSecs: NanoSecond, result: A) {
-    def relativeTo(decimals: Int = 2)(that: Result[A]) =
-      this.nanoSecs.relativeTo(decimals)(that.nanoSecs)
+  sealed abstract class MeasurementResult[A](result: A) {
+    def mediumNanoSecs: Long
+    def relativeTo(decimals: Int = 2)(that: MeasurementResult[A]) =
+      this.mediumNanoSecs.relativeTo(decimals)(that.mediumNanoSecs)
+    protected def toStringPrefix: String 
+    protected def optToStringParams = "" 
+    override def toString = s"$toStringPrefix($mediumNanoSecs ns, $result$optToStringParams)"
+  }
+  
+  case class SingleResult[A](nanoSecs: Long, result: A) extends MeasurementResult(result) {
+    def mediumNanoSecs = nanoSecs
+    protected def toStringPrefix = "Result" 
+  }
+  
+  case class Result[A](result: A)(times: ArrayBuffer[Long] = ArrayBuffer.empty) extends MeasurementResult(result) {
+    def this(result: A, firstNanoSecs: Long) { this(result)(ArrayBuffer(firstNanoSecs)) }
+    def mediumNanoSecs: Long = times.sum / times.size
+    def += (nanoSecs: Long): this.type = { this.times += nanoSecs; this }
+    def nanoSecs = times.iterator
+    protected def toStringPrefix = "Results" 
+    override protected def optToStringParams = s""", {${nanoSecs mkString " "}}""" 
   }
 
-  def measure[A](warmUp: Int = 1,
-                 repetitions: Int = 1)(block: => A): Result[A] = {
-    def once = {
-      val start = System.nanoTime
-      val res = block
-      val end = System.nanoTime
-      Result(end - start, res)
+  case class Results[A](list: List[Result[A]]) {
+    def relativeTimes(decimals: Int = 2): List[Float] = {
+      val mA = list.head
+      1f :: (list.tail map (r => r.relativeTo(decimals)(mA)))
     }
-    for (i <- 1 to warmUp)
-      block
-    val Result(firstTime, result) = once
-    var totalTime = firstTime
-    for (i <- 2 to repetitions)
-      once match {
-        case Result(t, r) => totalTime += t
-                             assert(r == result)
-      }
-    Result(totalTime / repetitions, result)
+  }
+  
+  private def requireEq[A](r1: A, r2: A): Unit = require(
+    r1 == r2,
+    s"'$r1' != '$r2' but blocks are expected to return the same result. Otherwise set requireEqualResults to false."
+  )
+
+  def once[A](block: => A): SingleResult[A] = {
+    val start = System.nanoTime
+    val res = block
+    val end = System.nanoTime
+    SingleResult(end - start, res)
   }
 
-  def time[A](warmUp: Int = 1,
-              repetitions: Int = 1)(block: => A): Long =
-    measure(warmUp, repetitions)(block).nanoSecs
+  private def once[A](byName: ByName[A]): SingleResult[A] = once(byName())
 
-  private def round(float: Float, decimals: Int) = { 
+  def measure[A](warmUp: Int = 1, repetitions: Int = 1)(block: => A): Result[A] =
+    measureAll[A](warmUp, repetitions)(block).list.head
+
+  def time[A](warmUp: Int = 1, repetitions: Int = 1)(block: => A): Long =
+    measure(warmUp, repetitions)(block).mediumNanoSecs
+
+  private def round(float: Float, decimals: Int) = {
     val fact = (10 pow decimals).toInt
     (float * fact).floor / fact
   }
-  
-  // relation of elapsed times a : b
-  def relativeTime[A](warmUp: Int = 1,
-                      repetitions: Int = 1,
-                      decimals: Int = 2)(a: => A, b: => A): Float = {
-    val touch: Boolean = if (warmUp > 0) { a; b; true } else false
-    val warmUpLeft = if (touch) warmUp - 1 else warmUp
-    
-    def m(x: => A): Result[A] = measure(warmUpLeft, repetitions)(x)
-    val (mA, mB) = (m(a), m(b))
-    assert(mA.result == mB.result)
-    mA.relativeTo(decimals)(mB)
+
+  // relation of elapsed times b : a
+  def relativeTime[A](warmUp: Int = 1, repetitions: Int = 1, decimals: Int = 2, requireEqualResults: Boolean = true)(
+                      a: => A, b: => A): Float =
+    measureAll[A](warmUp, repetitions, requireEqualResults)(a, b).list match {
+      case mA :: mB :: Nil => mB.relativeTo(decimals)(mA)
+      case x => throw new MatchError(x)
+    }
+
+  final class ByName[+A](x: => A) { def apply(): A = x }
+  implicit def toHolder[A](block: => A): ByName[A] = new ByName(block)
+
+  def measureAll[A](warmUp: Int = 1, repetitions: Int = 1, requireEqualResults: Boolean = true)(
+                    blocks: ByName[A]*): Results[A] = {
+    require(repetitions > 0, "'repetitions' must be positive")
+    for (i <- 1 to warmUp) blocks foreach once
+
+    val results: Array[Result[A]] = (blocks map once map (r => new Result(r.result, r.nanoSecs))).toArray 
+    val zippedBlocks = blocks.zipWithIndex 
+    for { i <- 1 until repetitions
+          zB <- zippedBlocks } {
+      val (b, j) = zB 
+      once(b) match {
+        case SingleResult(t, r) =>
+          results(j) += t
+          if (requireEqualResults) requireEq(r, results(j).result)
+      }
+    }
+    Results(results.toList)
   }
 
-  final class ByName[A](x: => A) { def apply(): A = x }
-  implicit def toHolder[A](block: => A) = new ByName(block)
-
-  def measureAll[A](warmUp: Int = 1,
-                    repetitions: Int = 1)(
-                    blocks: ByName[A]*): List[Result[A]] = {
-    val touch: Boolean = if (warmUp > 0) { blocks foreach (_()); true} else false
-    val warmUpLeft = if (touch) warmUp - 1 else warmUp
-    val results =
-      for( b <- blocks)
-      yield measure(warmUpLeft, repetitions)(b())
-    results.toList
-  }
-  
-  def relativeTimes[A](results: List[Result[A]], decimals: Int) = {
-    val mA = results.head
-    1f :: (results.tail map { res =>
-      assert(res.result == mA.result)
-      res.relativeTo(decimals)(mA)
-    })
-  }
-  
-  def relativeTimes[A](warmUp: Int = 1,
-                       repetitions: Int = 1,
-                       decimals: Int = 2)(
-                       blocks: ByName[A]*): List[Float] = {
-    val results = measureAll[A](warmUp, repetitions)(blocks: _*)
-    relativeTimes(results, decimals)
-  }
+  def relativeTimes[A](warmUp: Int = 1, repetitions: Int = 1, decimals: Int = 2, requireEqualResults: Boolean = true)(
+                       blocks: ByName[A]*): Seq[Float] =
+    measureAll[A](warmUp, repetitions, requireEqualResults)(blocks: _*).relativeTimes(decimals)
 }
--- core/src/test/scala/scalax/time/TMicroBenchmark.scala
+++ core/src/test/scala/scalax/time/TMicroBenchmark.scala
@@ -1,5 +1,6 @@
 package scalax.time
 
+import org.scalactic.Equality
 import org.scalatest.Spec
 import org.scalatest.Matchers
 
@@ -13,18 +14,68 @@ class TMicroBenchmarkTest
   
   import MicroBenchmark._
 
-  object `relative times` {
-    def `should reflect operation complexity` {
+  object `relativeTimes reflects` {
+    def `relative execution times` {
       val r = 1 to 20
-      val relTimes = relativeTimes()(
+      val relTimes = relativeTimes(warmUp = 2)(
         r.toList.sorted, 
-        r.toList.toArray.toList.sorted, 
-        r.toList.toSet.toArray.toList.sorted 
+        r.toList.sorted.toArray.toList.sorted, 
+        r.toList.sorted.toSet.toArray.toList.sorted 
       )
       relTimes sliding 2 foreach (_ match {
         case a :: b :: _ => a should be < b
-        case _ => throw new IllegalStateException
+        case x => throw new MatchError(x)
       })
     }
+  }
+  class FloatTolerance(maxDeviation: Float) extends Equality[Float] {
+    private def eq(a: Float, b: Float): Boolean = if (a > b) a < b * maxDeviation else a > b / maxDeviation
+    def areEqual(a: Float, b: Any) = b match {
+      case f: Float => eq(a, f)
+      case i: Int   => eq(a, i.toFloat)
+    } 
+  }
+  object `relativeTime roughly reflects` {
+    def `O(N) complexity of List.size` {
+      def fill(size: Int): (Int, List[Int]) = (size, List.fill(size)(0)) 
+      val (a, b) = (fill(100), fill(1000))
+      
+      implicit val tolerance = new FloatTolerance(2f)
+      val expected = b._1.toFloat / a._1.toFloat
+      val results = measureAll(warmUp = 5, repetitions = 10)(
+          a._2.size == a._1,
+          b._2.size == b._1)
+      val actual = results.relativeTimes()(1)
+      
+      actual should === (expected)
+    }
+  }
+  def `O(N) complexity of immutable Seq.size` {
+    import scala.collection.mutable.ArrayBuffer
+    val size = 1000
+    val vector = Vector.fill(size)(0)
+    val buffer = ArrayBuffer.fill(size)(0)
+
+    implicit val tolerance = new FloatTolerance(1.8f)
+    relativeTime()(buffer.size, vector.size) should === (size)
+  }
+  def `difference when traversing immutable vs. mutable Set` {
+    import scala.collection.mutable
+    val size = 10000
+    val array = Array.tabulate(size)(identity)
+    val sum = array.sum
+    val imm = Set(array: _*)
+    val m = mutable.Set(array: _*)
+
+    relativeTime(repetitions = 6)(m.sum == sum, imm.sum == sum) should be > (1.5f)
+  }
+  def `difference when traversing immutable Set vs. mutable BitSet` {
+    import scala.collection.mutable
+    val size = 10000
+    val array = Array.tabulate(size)(_ % (size / 10))
+    val s = Set(array: _*)
+    val b = mutable.BitSet(array: _*)
+
+    relativeTime(warmUp = 20, repetitions = 6)(b.sum, s.sum) should be > (2f)
   }
 }
