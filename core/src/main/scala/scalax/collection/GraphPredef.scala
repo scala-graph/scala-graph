e66d0d488d6b8ab8a34e19758beff24d950f56bc
Peter Empen <postmaster@scala-graph.org>
2018-12-09 16:08:27 +0100

    reshaped edge implicits

--- core/src/main/scala/scalax/collection/GraphPredef.scala
+++ core/src/main/scala/scalax/collection/GraphPredef.scala
@@ -4,7 +4,7 @@ import scala.annotation.unchecked.{uncheckedVariance => uV}
 import scala.language.{higherKinds, implicitConversions}
 import scala.collection.{AbstractIterable, SeqFacade}
 
-import GraphEdge.EdgeLike
+import GraphEdge._
 
 /** This object serves as a container for several `Graph`-related definitions like parameter-types and implicit conversions.
   *
@@ -36,7 +36,6 @@ object GraphPredef {
   implicit def edgeSetToSeq[N, E[X] <: EdgeLike[X]](edges: Graph[N, E]#EdgeSetT): Seq[Graph[N, E]#EdgeT] =
     new SeqFacade(edges)
 
-  //--------------------------------------------------------------------------------------------------------------------
   /** Represents parameters that are accepted when calling `Graph(...)`.
     *
     * @tparam N  the type of the nodes (vertices)
@@ -51,80 +50,78 @@ object GraphPredef {
   trait OuterEdge[+N, +E[X <: N @uV] <: EdgeLike[X]] extends OuterElem[N, E] { this: E[N @uV] =>
   }
 
-  @inline implicit def anyToNode[N](n: N): OuterNode[N] = new OuterNode(n)
-}
+  @inline implicit def anyToNode[N](n: N): OuterNode[N] = OuterNode(n)
 
-import GraphEdge._
+  abstract class AbstractHyperEdgeImplicits[E[N] <: AbstractHyperEdge[N], C <: HyperEdgeCompanion[E]](companion: C) {
 
-abstract class AbstractHyperEdgeImplicits[E[N] <: AbstractHyperEdge[N], C <: HyperEdgeCompanion[E]](companion: C) {
+    trait AnyToEdge[N] extends Any {
+      def n1: N
+      def ~~[NN >: N](n2: NN): E[NN] = companion(n1, n2)
+    }
 
-  trait AnyToEdge[N] extends Any {
-    def n1: N
-    def ~~[NN >: N](n2: NN): E[NN] = companion(n1, n2)
+    trait EdgeToEdge[N] extends Any {
+      def e1: E[N]
+      def ~~[NN >: N](n: NN): E[NN] = companion[NN](e1.ends ++ (n :: Nil))
+    }
   }
 
-  trait EdgeToEdge[N] extends Any {
-    def e1: E[N]
-    def ~~[NN >: N](n: NN): E[NN] = companion[NN](e1.ends ++ (n :: Nil))
-  }
-}
-
-object HyperEdgeImplicits extends AbstractHyperEdgeImplicits[HyperEdge, HyperEdge.type](HyperEdge) {
-  implicit class AnyToEdge[N](override val n1: N)             extends AnyVal with super.AnyToEdge[N]
-  implicit class EdgeToEdge[N](override val e1: HyperEdge[N]) extends AnyVal with super.EdgeToEdge[N]
-}
-
-object OrderedHyperEdgeImplicits
-    extends AbstractHyperEdgeImplicits[OrderedHyperEdge, OrderedHyperEdge.type](OrderedHyperEdge) {
-  implicit class AnyToEdge[N](override val n1: N)                    extends AnyVal with super.AnyToEdge[N]
-  implicit class EdgeToEdge[N](override val e1: OrderedHyperEdge[N]) extends AnyVal with super.EdgeToEdge[N]
-}
-
-abstract class AbstractDiHyperEdgeImplicits[E[N] <: AbstractDiHyperEdge[N], C <: DiHyperEdgeCompanion[E]](
-    companion: C) {
-
-  trait AnyToEdge[N] extends Any {
-    def source: N
-    def ~~>[NN >: N](target: NN): E[NN]            = companion(Iterable(source), Iterable(target))
-    def ~~>[NN >: N](targets: Iterable[NN]): E[NN] = companion(Iterable(source), targets)
+  object HyperEdgeImplicits extends AbstractHyperEdgeImplicits[HyperEdge, HyperEdge.type](HyperEdge) {
+    implicit class AnyToEdge[N](override val n1: N)             extends AnyVal with super.AnyToEdge[N]
+    implicit class EdgeToEdge[N](override val e1: HyperEdge[N]) extends AnyVal with super.EdgeToEdge[N]
   }
 
-  trait IterableToEdge[N] extends Any {
-    def sources: Iterable[N]
-    def ~~>[NN >: N](target: NN): E[NN]            = companion(sources, Iterable(target))
-    def ~~>[NN >: N](targets: Iterable[NN]): E[NN] = companion(sources, targets)
+  object OrderedHyperEdgeImplicits
+      extends AbstractHyperEdgeImplicits[OrderedHyperEdge, OrderedHyperEdge.type](OrderedHyperEdge) {
+    implicit class AnyToEdge[N](override val n1: N)                    extends AnyVal with super.AnyToEdge[N]
+    implicit class EdgeToEdge[N](override val e1: OrderedHyperEdge[N]) extends AnyVal with super.EdgeToEdge[N]
   }
-}
 
-object DiHyperEdgeImplicits extends AbstractDiHyperEdgeImplicits[DiHyperEdge, DiHyperEdge.type](DiHyperEdge) {
-  implicit class AnyToEdge[N](override val source: N)             extends AnyVal with super.AnyToEdge[N]
-  implicit class EdgeToEdge[N](override val sources: Iterable[N]) extends AnyVal with super.IterableToEdge[N]
-}
+  abstract class AbstractDiHyperEdgeImplicits[E[N] <: AbstractDiHyperEdge[N], C <: DiHyperEdgeCompanion[E]](
+      companion: C) {
 
-object OrderedDiHyperEdgeImplicits
-    extends AbstractDiHyperEdgeImplicits[OrderedDiHyperEdge, OrderedDiHyperEdge.type](OrderedDiHyperEdge) {
-  implicit class AnyToEdge[N](override val source: N)             extends AnyVal with super.AnyToEdge[N]
-  implicit class EdgeToEdge[N](override val sources: Iterable[N]) extends AnyVal with super.IterableToEdge[N]
-}
+    trait AnyToEdge[N] extends Any {
+      def source: N
+      def ~~>[NN >: N](target: NN): E[NN]            = companion(Iterable(source), Iterable(target))
+      def ~~>[NN >: N](targets: Iterable[NN]): E[NN] = companion(Iterable(source), targets)
+    }
 
-abstract class AbstractEdgeImplicits[E[N] <: EdgeLike[N] with AbstractEdge[N], C <: EdgeCompanion[E]](companion: C) {
-  trait AnyToEdge[N] extends Any {
+    trait IterableToEdge[N] extends Any {
+      def sources: Iterable[N]
+      def ~~>[NN >: N](target: NN): E[NN]            = companion(sources, Iterable(target))
+      def ~~>[NN >: N](targets: Iterable[NN]): E[NN] = companion(sources, targets)
+    }
+  }
+
+  object DiHyperEdgeImplicits extends AbstractDiHyperEdgeImplicits[DiHyperEdge, DiHyperEdge.type](DiHyperEdge) {
+    implicit class AnyToEdge[N](override val source: N)             extends AnyVal with super.AnyToEdge[N]
+    implicit class EdgeToEdge[N](override val sources: Iterable[N]) extends AnyVal with super.IterableToEdge[N]
+  }
+
+  object OrderedDiHyperEdgeImplicits
+      extends AbstractDiHyperEdgeImplicits[OrderedDiHyperEdge, OrderedDiHyperEdge.type](OrderedDiHyperEdge) {
+    implicit class AnyToEdge[N](override val source: N)             extends AnyVal with super.AnyToEdge[N]
+    implicit class EdgeToEdge[N](override val sources: Iterable[N]) extends AnyVal with super.IterableToEdge[N]
+  }
+
+  trait AbstractEdgeImplicits[N, E[N] <: EdgeLike[N] with AbstractEdge[N], C <: EdgeCompanion[E]] {
+    protected def companion: C
     def n1: N
     def ~[NN >: N](n2: NN): E[NN] = companion(n1, n2)
   }
-}
 
-object UnDiEdgeImplicits extends AbstractEdgeImplicits[UnDiEdge, UnDiEdge.type](UnDiEdge) {
-  implicit class AnyToEdge[N](override val n1: N) extends AnyVal with super.AnyToEdge[N]
-}
+  implicit class ToUnDiEdge[N](override val n1: N)
+      extends AnyVal
+      with AbstractEdgeImplicits[N, UnDiEdge, UnDiEdge.type] {
+    protected def companion = UnDiEdge
+  }
 
-abstract class AbstractDiEdgeImplicits[E[N] <: AbstractDiEdge[N], C <: EdgeCompanion[E]](companion: C) {
-  trait AnyToEdge[N] extends Any {
+  trait AbstractDiEdgeImplicits[N, E[N] <: AbstractDiEdge[N], C <: EdgeCompanion[E]] {
+    protected def companion: C
     def source: N
     def ~>[NN >: N](target: NN): E[NN] = companion(source, target)
   }
-}
 
-object DiEdgeImplicits extends AbstractDiEdgeImplicits[DiEdge, DiEdge.type](DiEdge) {
-  implicit class AnyToEdge[N](override val source: N) extends AnyVal with super.AnyToEdge[N]
+  implicit final class ToDiEdge[N](val source: N) extends AnyVal with AbstractDiEdgeImplicits[N, DiEdge, DiEdge.type] {
+    protected def companion = DiEdge
+  }
 }
