188d3e8c7c159861621d68915c81fe6b6cc6fa6c
Peter Empen <postmaster@scala-graph.org>
2019-11-10 20:40:17 +0100

    versioning

--- project/Version.scala
+++ project/Version.scala
@@ -5,10 +5,10 @@ object Version {
   private val isSnapshot = false
   private def snapshot   = if (isSnapshot) "-SNAPSHOT" else ""
   private val major      = "1.13"
-  val highest            = s"$major.0$snapshot"
-  val core               = s"$major.0$snapshot"
+  val highest            = s"$major.1$snapshot"
+  val core               = s"$major.1$snapshot"
   val constrained        = s"$major.0$snapshot"
-  val dot                = s"$major.0$snapshot"
-  val json               = s"$major.0$snapshot"
+  val dot                = s"1.12.1$snapshot"
+  val json               = s"1.12.1$snapshot"
   val misc               = s"$major.0$snapshot"
 }
