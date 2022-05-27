33cfeb10df41122275001bf02d2a0cc13d71baec
Peter Empen <postmaster@scala-graph.org>
2022-05-27 20:15:03 +0200

    fix diff

--- core/src/main/scala/scalax/collection/GraphBase.scala
+++ core/src/main/scala/scalax/collection/GraphBase.scala
@@ -414,7 +414,7 @@ trait GraphBase[N, E <: EdgeLike[N], +This[X, Y <: EdgeLike[X]] <: GraphBase[X, 
 
     def draw(random: Random): NodeT
 
-    def diff(that: AnySet[NodeT]): AnySet[NodeT] = this diff that
+    def diff(that: AnySet[NodeT]): AnySet[NodeT] = this -- that
   }
 
   /** The node (vertex) set of this `Graph` commonly referred to as V(G).
@@ -593,7 +593,7 @@ trait GraphBase[N, E <: EdgeLike[N], +This[X, Y <: EdgeLike[X]] <: GraphBase[X, 
       }
     }
 
-    def diff(that: AnySet[EdgeT]): AnySet[EdgeT] = this diff that
+    def diff(that: AnySet[EdgeT]): AnySet[EdgeT] = this -- that
   }
 
   /** The edge set of this `Graph` commonly referred to as E(G).
