0c7e1f10e42344ec9e73e83f68044b1f3bff4500
Peter Empen <peter.empen@commercetools.com>
2022-10-01 21:43:14 +0200

    output rendered graph in "given"

--- core/src/test/scala/scalax/collection/visualization/Visualizer.scala
+++ core/src/test/scala/scalax/collection/visualization/Visualizer.scala
@@ -1,12 +1,10 @@
 package scalax.collection.visualization
 
-/*
-import scala.util.{Failure, Success}
 import org.scalatest.exceptions.TestFailedException
- */
 
 import scalax.collection.generic.Edge
 import scalax.collection.AnyGraph
+import scalax.collection.ToString._
 
 /** Scalatest support for graph visualization in case of failures.
   *
@@ -20,31 +18,37 @@ import scalax.collection.AnyGraph
   */
 trait Visualizer /*extends Drawable*/ {
 
-  final def given[N, E <: Edge[N]](graph: AnyGraph[N, E])(test: AnyGraph[N, E] => Unit): Unit =
-    /*
-    def reThrow(ex: TestFailedException, secondLine: String) =
+  final def given[N, E <: Edge[N]](graph: AnyGraph[N, E])(test: AnyGraph[N, E] => Unit): Unit = {
+
+    def reThrow(ex: TestFailedException, messageExtension: String) =
       throw ex.modifyMessage(_.map { testMessage =>
         s"""$testMessage
-           |$secondLine
-       """.stripMargin
+           |------------ given ------------
+           |$messageExtension
+           |-------------------------------""".stripMargin
       })
 
-    try */ test(graph) /*
+    try test(graph)
     catch {
       case ex: TestFailedException =>
+        /*
         makeImage(
           graph,
           path = "log/",
           name = (ex.failedCodeFileName match {
             case Some(fileName) => fileName
-            case None           => "failed_test"
+            case None => "failed_test"
           }) + (ex.failedCodeLineNumber match {
             case Some(number) => "_line" + number.toString
-            case None         => ""
+            case None => ""
           }) + ".png"
         ) match {
           case Success(f) => reThrow(ex, s"The graph image is available at file://${f.getAbsolutePath}")
           case Failure(e) => reThrow(ex, s"Graph image generation failed with `${e.getMessage}`.")
         }
-    } */
+         */
+        val small = graph.edges.size < 12 && graph.edges.toString.length < 120
+        reThrow(ex, graph.render(if (small) SetsOnSeparateLines() else SetElemsOnSeparateLines()))
+    }
+  }
 }
