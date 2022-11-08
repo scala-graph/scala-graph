c591fded45c181ef47a8b77c575592f55dcdfba3
Peter Empen <peter.empen@commercetools.com>
2022-11-08 09:52:46 +0100

    drop suite in spec

--- core/src/test/scala/scalax/collection/MappingHyperSpec.scala
+++ core/src/test/scala/scalax/collection/MappingHyperSpec.scala
@@ -3,61 +3,45 @@ package scalax.collection
 import org.scalatest.refspec.RefSpec
 import org.scalatest.matchers.should.Matchers
 
-import org.scalatest.Suites
-
 import scalax.collection.OneOrMore.{more, one}
 import scalax.collection.OuterImplicits._
-import scalax.collection.generic._
 import scalax.collection.hyperedges._
-import scalax.collection.generic.GenericGraphCoreFactory
+import scalax.collection.immutable.Graph
 
-import MappingHyperSpec._
+class MappingHyperSpec extends RefSpec with Matchers {
 
-class MappingHyperSpec
-    extends Suites(
-      new Common[immutable.Graph](immutable.Graph),
-      new Common[mutable.Graph](mutable.Graph)
-    )
+  object `mapping a generic hypergraph you can` {
+    val g = Graph(1 ~~ 2 ~~ 3, 3 ~~ 4 ~~ 1)
 
-object MappingHyperSpec {
-  private class Common[CC[N, E <: Edge[N]] <: AnyGraph[N, E] with GraphLike[N, E, CC]](
-      val factory: GenericGraphCoreFactory[CC]
-  ) extends RefSpec
-      with Matchers {
+    def `map by identity`: Unit =
+      g.map(identity) shouldEqual g
 
-    object `mapping a generic hypergraph you can` {
-      val g = factory(1 ~~ 2 ~~ 3, 3 ~~ 4 ~~ 1)
+    def `map nodes`: Unit =
+      g.map(_.toString) shouldEqual Graph("1" ~~ "2" ~~ "3", "3" ~~ "4" ~~ "1")
 
-      def `map by identity`: Unit =
-        g.map(identity) shouldEqual g
+    def `map both nodes and edges`: Unit =
+      // increment node values and, mapping edges, add only the first two ends
+      g.mapHyper(
+        fNode = _.outer + 1,
+        fHyperEdge = (newEnds: Several[Int]) => newEnds(0) ~~ newEnds(1)
+      ) shouldEqual Graph(2 ~~ 3, 4 ~~ 5)
+  }
 
-      def `map nodes`: Unit =
-        g.map(_.toString) shouldEqual factory("1" ~~ "2" ~~ "3", "3" ~~ "4" ~~ "1")
+  object `mapping a generic directed hypergraph you can` {
+    val g = Graph(more(1, 2) ~~> one(3), one(3) ~~> more(4, 1))
 
-      def `map both nodes and edges`: Unit =
-        // increment node values and, mapping edges, add only the first two ends
-        g.mapHyper(
-          fNode = _.outer + 1,
-          fHyperEdge = (newEnds: Several[Int]) => newEnds(0) ~~ newEnds(1)
-        ) shouldEqual factory(2 ~~ 3, 4 ~~ 5)
-    }
+    def `map by identity`: Unit =
+      g.map(identity) shouldEqual g
 
-    object `mapping a generic directed hypergraph you can` {
-      val g = factory(more(1, 2) ~~> one(3), one(3) ~~> more(4, 1))
+    def `map nodes`: Unit =
+      g.map(_.toString) shouldEqual Graph(more("1", "2") ~~> one("3"), one("3") ~~> more("4", "1"))
 
-      def `map by identity`: Unit =
-        g.map(identity) shouldEqual g
-
-      def `map nodes`: Unit =
-        g.map(_.toString) shouldEqual factory(more("1", "2") ~~> one("3"), one("3") ~~> more("4", "1"))
-
-      def `map both nodes and edges`: Unit =
-        // increment node values and, mapping edges, add sources to targets ends
-        g.mapDiHyper(
-          fNode = _.outer + 1,
-          fDiHyperEdge =
-            (newSources: OneOrMore[Int], newTargets: OneOrMore[Int]) => newSources ~~> (newTargets ++ newSources)
-        ) shouldEqual factory(more(2, 3) ~~> more(4, 2, 3), one(4) ~~> more(5, 2, 4))
-    }
+    def `map both nodes and edges`: Unit =
+      // increment node values and, mapping edges, add sources to targets ends
+      g.mapDiHyper(
+        fNode = _.outer + 1,
+        fDiHyperEdge =
+          (newSources: OneOrMore[Int], newTargets: OneOrMore[Int]) => newSources ~~> (newTargets ++ newSources)
+      ) shouldEqual Graph(more(2, 3) ~~> more(4, 2, 3), one(4) ~~> more(5, 2, 4))
   }
 }
