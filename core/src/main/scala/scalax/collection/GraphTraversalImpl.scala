8fa03c87ae28e653d9c6b17b07f469ddfb2aca06
Mario Càllisto <mario.callisto@gmail.com>
2024-06-18 09:03:54 +0200

    Improved explicit types

--- core/src/main/scala/scalax/collection/GraphTraversalImpl.scala
+++ core/src/main/scala/scalax/collection/GraphTraversalImpl.scala
@@ -121,10 +121,10 @@ trait GraphTraversalImpl[N, E <: Edge[N]] extends GraphTraversal[N, E] with Trav
     )(_ => edges.slice(0, edges.size - 1))
 
     def result(): Walk = new Walk {
-      val nodes                  = self.nodes
-      def edges: Iterable[EdgeT] = resultEdges
-      val startNode: NodeT       = start
-      val endNode                = nodes(nodes.size - 1)
+      val nodes: IndexedSeq[NodeT] = self.nodes
+      def edges: IndexedSeq[EdgeT] = resultEdges
+      val startNode: NodeT         = start
+      val endNode                  = nodes(nodes.size - 1)
     }
   }
 
