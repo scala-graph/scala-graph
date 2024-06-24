11e9f2efc49e2bc392961ed30ceddedccf3ff5fc
Mario Càllisto <mario.callisto@gmail.com>
2024-06-24 11:27:40 +0200

    rewritten as while, since do while is no longer available in Scala 3

--- core/src/main/scala/scalax/collection/generator/RandomGraph.scala
+++ core/src/main/scala/scalax/collection/generator/RandomGraph.scala
@@ -333,8 +333,8 @@ class RandomGraph[N, E <: Edge[N], G[X, Y <: Edge[X]] <: AnyGraph[X, Y] with Gra
       }
 
     var edge  = new RandomEdge(weightFactory, labelFactory)
-    var added = false
-    do {
+    var added = true
+    while (added && edge.isDefined) {
       val mayFinish = degrees.mayFinish
       added = addExact[E](
         nrToAdd = 1,
@@ -348,7 +348,7 @@ class RandomGraph[N, E <: Edge[N], G[X, Y <: Edge[X]] <: AnyGraph[X, Y] with Gra
         gentle = mayFinish
       )
       if (added) edge = new RandomEdge(weightFactory, labelFactory)
-    } while (added && edge.isDefined)
+    }
   }
 
   /** Returns a random graph of the specified type and with the metrics passed to the enclosing class.
