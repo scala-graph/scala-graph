d4092f42c6b0d1706c0e0b9b90cd69d921f053e8
Mario Càllisto <mario.callisto@gmail.com>
2024-06-29 13:59:00 +0200

    Added working EqualitySpec

---
+++ coreTestScala3/src/test/scala/scalax/collection/EqualitySpec.scala
@@ -0,0 +1,58 @@
+package scalax.collection
+
+import org.scalatest.Suites
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.refspec.RefSpec
+import scalax.collection.OuterImplicits.*
+import scalax.collection.edges.*
+import scalax.collection.generic.{Edge, GenericGraphCoreFactory}
+
+class EqualitySpec
+    extends Suites(
+      new Equality[immutable.Graph](immutable.Graph),
+      new Equality[mutable.Graph](mutable.Graph),
+      new EqualityMixed
+    )
+
+private class Equality[CC[N, E <: Edge[N]] <: AnyGraph[N, E] with GraphLike[N, E, CC]](
+    val factory: GenericGraphCoreFactory[CC]
+) extends RefSpec
+    with Matchers {
+
+  private val seq_1_3   = Seq(1, 3)
+  private val gInt_1_3  = factory(seq_1_3.toOuterElems[DiEdge[Int]]: _*)
+  private val gString_A = factory("A")
+
+  def `Eq ` : Unit = {
+    factory[Int, Nothing]() shouldEqual factory[Int, DiEdge]()
+    gInt_1_3 shouldEqual factory(1, 3)
+    gString_A shouldEqual factory("A")
+
+    factory() shouldNot be(factory(1))
+    gInt_1_3 shouldNot be(factory(2, 3))
+    gString_A shouldNot be(factory("B"))
+
+    gInt_1_3 shouldEqual immutable.Graph(1) + 3
+  }
+}
+
+private class EqualityMixed extends RefSpec with Matchers {
+
+  val oEdgesG = List(1 ~ 2, 2 ~ 3, 2 ~ 4, 3 ~ 5, 4 ~ 5)
+  val oEdgesH = List(3 ~ 4, 3 ~ 5, 4 ~ 6, 5 ~ 6)
+
+  val (iFactory, mFactory) = (immutable.Graph, mutable.Graph)
+
+  def initG = (iFactory(oEdgesG: _*), mFactory(oEdgesG: _*))
+  def initH = (iFactory(oEdgesH: _*), mFactory(oEdgesH: _*))
+
+  object `equals works properly` {
+    def `over immutable and mutable graphs`: Unit = {
+      val (iG, mG) = initG
+      iG should ===(mG)
+
+      val (iH, mH) = initH
+      iH should ===(mH)
+    }
+  }
+}
