c1d19aeca1c72eb466a5a26a51aa4c287f4b04b4
Peter Empen <postmaster@scala-graph.org>
2024-07-20 19:24:03 +0200

    rename tests files, copy demo files

---
+++ coreTestScala3/src/test/scala/demo/GraphTestDemo.scala
@@ -0,0 +1,181 @@
+package demo
+
+import scalax.collection.edges.*
+import scalax.collection.mutable.Graph
+
+/** Includes the examples given on [[http://www.scala-graph.org/guides/test.html
+  *  Test Utilities]].
+  */
+object GraphTestDemo extends App {
+
+  import scalax.collection.generator.*
+
+  object PersonData {
+    val firstNames     = Vector("Alen", "Alice", "Bob", "Jack", "Jim", "Joe", "Kate", "Leo", "Tim", "Tom")
+    val firstNamesSize = firstNames.size
+
+    val surnames     = Vector("Bell", "Brown", "Clark", "Cox", "King", "Lee", "Moore", "Ross", "Smith", "Wong")
+    val surnamesSize = surnames.size
+
+    def order          = firstNamesSize * surnamesSize / 10
+    def degrees        = new NodeDegreeRange(2, order / 2)
+    val maxYearOfBirth = 2010
+  }
+
+  object RG {
+    // working with random graph generators -----------------------------------------------
+
+    // obtaining generators for graphs with predefined metrics
+    val predefined = RandomGraph.tinyConnectedIntDi(Graph)
+    val tinyGraph  = predefined.draw // Graph[Int,DiEdge]
+
+    // setting individual graph metrics while keeping metrics constraints in mind
+    object sparse_1000_Int extends RandomGraph.IntFactory {
+      val order              = 1000
+      val nodeDegrees        = NodeDegreeRange(1, 10)
+      override def connected = false
+    }
+    val randomSparse = RandomGraph.fromMetrics[Int, UnDiEdge[Int], Graph](Graph, sparse_1000_Int, Set(UnDiEdge))
+    val sparseGraph  = randomSparse.draw // Graph[Int,UnDiEdge]
+
+    // TODO obtain generators for graphs with typed edges
+
+    case class Person(name: String, yearOfBirth: Int)
+    object Person {
+      import PersonData.*
+      private val r = new scala.util.Random
+
+      def drawName: String = {
+        def drawFirstName: String = firstNames(r.nextInt(firstNamesSize))
+        def drawSurame: String    = surnames(r.nextInt(surnamesSize))
+
+        s"$drawFirstName, $drawSurame"
+      }
+
+      def drawYearOfBirth = maxYearOfBirth - r.nextInt(100)
+    }
+
+    val randomMixedGraph =
+      RandomGraph.fromMetrics[Person, UnDiEdge[Person], Graph](
+        Graph,
+        new RandomGraph.Metrics[Person] {
+          val order           = PersonData.order
+          val nodeDegrees     = PersonData.degrees
+          def nodeGen: Person = Person(Person.drawName, Person.drawYearOfBirth)
+        },
+        Set(UnDiEdge) // TODO LDiEdge
+      )
+    val mixedGraph = randomMixedGraph.draw
+    /*
+    println(mixedGraph)
+    Graph(
+        Person(Alice, Smith,1967),
+        Person(Kate, Ross,1921),
+        Person(Leo, Bell,2008),
+        Person(Leo, Smith,1983),
+        ...,
+        Person(Alice, Smith,1967)~>Person(Kate, Ross,1921) 'C,
+        Person(Leo, Bell,2008)~Person(Leo, Smith,1983),
+        ...
+    )
+     */
+  }
+
+  object GG {
+    import org.scalacheck.{Arbitrary, Gen}
+    import Arbitrary.arbitrary
+    import org.scalacheck.Prop.forAll
+
+    // working with org.scalacheck.Arbitrary graphs ---------------------------------------
+
+    // obtaining Arbitrary instances for graphs with predefined metrics
+    type IntDiGraph = Graph[Int, DiEdge[Int]]
+    implicit val arbitraryTinyGraph: Arbitrary[Graph[Int, DiEdge[Int]]] = GraphGen.tinyConnectedIntDi[Graph](Graph)
+
+    val properTiny = forAll(arbitrary[IntDiGraph]) { g: IntDiGraph =>
+      g.order == GraphGen.TinyInt.order
+    }
+    properTiny.check()
+
+    // setting individual graph metrics for Arbitrary instances
+    // while keeping metrics constraints in mind
+    object Sparse_1000_Int extends GraphGen.Metrics[Int] {
+      val order              = 1000
+      val nodeDegrees        = NodeDegreeRange(1, 10)
+      def nodeGen: Gen[Int]  = Gen.choose(0, 10 * order)
+      override def connected = false
+    }
+
+    type IntUnDiGraph = Graph[Int, UnDiEdge[Int]]
+    implicit val arbitrarySparseGraph: Arbitrary[Graph[Int, UnDiEdge[Int]]] = Arbitrary {
+      GraphGen.fromMetrics[Int, UnDiEdge[Int], Graph](Graph, Sparse_1000_Int, Set(UnDiEdge)).apply
+    }
+
+    val properSparse = forAll(arbitrary[IntUnDiGraph]) { g: IntUnDiGraph =>
+      g.order == Sparse_1000_Int.order
+    }
+    properSparse.check()
+
+    // TODO obtain Arbitrary instances to generate graphs with typed edges
+
+    case class Person(name: String, yearOfBirth: Int)
+    object Person {
+      import PersonData.*
+
+      def firstNameGen: Gen[String] = Gen.oneOf(firstNames)
+      def surameGen: Gen[String]    = Gen.oneOf(surnames)
+
+      def nameGen: Gen[String] = Gen.resultOf((firstName: String, surname: String) => s"$firstName, $surname")(
+        Arbitrary(firstNameGen),
+        Arbitrary(surameGen)
+      )
+
+      def yearOfBirthGen: Gen[Int] = Gen.choose(maxYearOfBirth - 100, maxYearOfBirth)
+    }
+
+    object MixedMetrics extends GraphGen.Metrics[Person] {
+      val order       = PersonData.order
+      val nodeDegrees = PersonData.degrees
+      def nodeGen: Gen[Person] = Gen.resultOf((name: String, year: Int) => Person(name, year))(
+        Arbitrary(Person.nameGen),
+        Arbitrary(Person.yearOfBirthGen)
+      )
+    }
+
+    type Mixed = Graph[Person, UnDiEdge[Person]]
+    implicit val arbitraryMixedGraph: Arbitrary[Graph[Person, UnDiEdge[Person]]] = Arbitrary {
+      GraphGen
+        .fromMetrics[Person, UnDiEdge[Person], Graph](Graph, MixedMetrics, Set(UnDiEdge /*, TODO LDiEdge*/ ))
+        .apply
+    }
+
+    val properMixedGraph = forAll(arbitrary[Mixed]) { g: Mixed =>
+      g.order == MixedMetrics.order
+    }
+    properMixedGraph.check()
+    // println(arbitraryMixedGraph.arbitrary.sample)
+
+    // Integrating with ScalaTest, limiting the minimum # of successful tests
+    import org.scalatest.matchers.should.Matchers
+    import org.scalatest.refspec.RefSpec
+    import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+    class TGraphGenTest extends RefSpec with Matchers with ScalaCheckPropertyChecks {
+
+      implicit val config: PropertyCheckConfiguration =
+        PropertyCheckConfiguration(minSuccessful = 5, maxDiscardedFactor = 1.0)
+
+      object `generated Tiny graph` {
+        implicit val arbitraryTinyGraph: Arbitrary[Graph[Int, DiEdge[Int]]] = GraphGen.tinyConnectedIntDi[Graph](Graph)
+
+        def `should conform to tiny metrics`: Unit =
+          forAll(arbitrary[IntDiGraph]) { g: IntDiGraph =>
+            g.order should equal(GraphGen.TinyInt.order)
+          }
+      }
+    }
+  }
+
+  RG
+  GG
+}
---
+++ coreTestScala3/src/test/scala/demo/subset.scala
@@ -0,0 +1,88 @@
+package demo
+
+import scala.collection.immutable.ArraySeq
+import scala.reflect.ClassTag
+import scala.util.chaining.*
+
+protected trait SubsetOps[A, +S <: Set[A] with Singleton] extends Set[A] {
+  def superset: S
+  def set: Set[A]
+
+  def contains(elem: A): Boolean = set(elem)
+  def iterator: Iterator[A]      = set.iterator
+  override def toString: String  = s"${getClass.getSimpleName}(${set mkString ", "}) out of ${superset.size}"
+}
+
+/** Includes a `superset` and a `set` guaranteeing that `set` is a subset of `superset`.
+  *
+  * @tparam A type of the elements of both `superset` and `set`
+  * @tparam S singleton type denoting `superset`
+  */
+case class Subset[A, +S <: Set[A] with Singleton] private (superset: S, set: Set[A]) extends SubsetOps[A, S] {
+
+  def incl(elem: A): Subset[A, S] =
+    if (superset(elem) && !set(elem)) Subset(superset, set incl elem)
+    else this
+
+  def excl(elem: A): Subset[A, S] = Subset(superset, set excl elem)
+}
+
+object Subset {
+  def apply[A](superset: Set[A])(elems: A*): Subset[A, superset.type] =
+    new Subset(superset, elems.iterator.filter(superset.apply).toSet)
+
+  def byFilter[A](superset: Set[A])(predicate: A => Boolean): Subset[A, superset.type] =
+    new Subset(superset, superset.filter(predicate))
+}
+
+/** Includes a `superset`, a `seq` and a `set` guaranteeing that `set` is a subset of `superset` and
+  * `seq` has the same elements as `set` ordered on creation time.
+  *
+  * @tparam A type of the elements of both `superset` and `set`
+  * @tparam S singleton type denoting `superset`
+  */
+case class OrderedSubset[A, +S <: Set[A] with Singleton] private (superset: S, ordered: ArraySeq[A], set: Set[A])
+    extends SubsetOps[A, S] {
+
+  def incl(elem: A): OrderedSubset[A, S] =
+    if (superset(elem) && !set(elem))
+      OrderedSubset(superset, ordered appended elem, set incl elem)
+    else this
+
+  def excl(elem: A): OrderedSubset[A, S] =
+    ordered.indexOf(elem) pipe { idx =>
+      if (idx == -1) this
+      else OrderedSubset(superset, ordered.patch(idx, elem :: Nil, 1), set excl elem)
+    }
+}
+
+object OrderedSubset {
+  def apply[A](superset: Set[A])(elems: A*)(implicit tag: ClassTag[A]): OrderedSubset[A, superset.type] =
+    elems.foldLeft((ArraySeq.empty[A], Set.empty[A])) {
+      case (cum @ (_, set), elem) if !superset(elem) || set(elem) => cum
+      case ((seq, set), elem)                                     => (seq appended elem, set incl elem)
+    } match {
+      case (seq, set) => new OrderedSubset(superset, seq, set)
+    }
+}
+
+/** Includes a `superset` and a single element `set` guaranteeing that `set` is a subset of `superset`.
+  *
+  * @tparam A type of the elements of both `superset` and `set`
+  * @tparam S singleton type denoting `superset`
+  */
+case class OneOf[A, +S <: Set[A] with Singleton] private (superset: S, one: A) {
+  override def toString: String = s"One($one) out of ${superset.size}"
+}
+
+object OneOf {
+  def apply[A](superset: Set[A])(elem: A): Option[OneOf[A, superset.type]] =
+    if (superset(elem)) Some(new OneOf(superset, elem))
+    else None
+}
+
+/** Wraps an `immutable.Set` by also capturing the underlying set as a singleton type.
+  * Useful if you need to enforce `Superset` and `Subset` instances to use the same superset singleton.
+  * Gets probably obsolete in Scala 3.
+  */
+case class Superset[A, +S <: Set[A] with Singleton](set: Set[A] with S)
