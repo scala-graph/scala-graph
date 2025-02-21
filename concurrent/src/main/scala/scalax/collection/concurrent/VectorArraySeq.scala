231d7fd557b8e5365e41261127404eca93d85761
Peter Empen <peter.empen@commercetools.com>
2025-02-21 17:51:39 +0100

    'appended', single threaded

--- concurrent/src/main/scala/scalax/collection/concurrent/VectorArraySeq.scala
+++ concurrent/src/main/scala/scalax/collection/concurrent/VectorArraySeq.scala
@@ -3,20 +3,37 @@ package scalax.collection.concurrent
 import scala.math.max
 
 private[concurrent] type Size = Int
-private[concurrent] case class Chunk[A <: AnyRef, C](prefix: C, var used: Size, array: Array[A]):
+
+private[concurrent] case class Chunk[A, C](prefix: C, var used: Size, array: Array[A]):
   def elem(globalIndex: Index, chunkSize: Size): A =
     val i = globalIndex % chunkSize
     if i < used then array(i)
     else throw new IndexOutOfBoundsException
 
+private[concurrent] object Chunk:
+  def empty[A, C](prefix: C)(implicit size: Size): Chunk[A, C] =
+    Chunk[A, C](prefix, 0, new Array[AnyRef](size).asInstanceOf[Array[A]])
+
+  def apply[A, C](prefix: C, elem: A)(implicit size: Size): Chunk[A, C] =
+    val arr = new Array[AnyRef](size).asInstanceOf[Array[A]]
+    arr(0) = elem
+    Chunk[A, C](prefix, 1, arr)
+
 /** Growing only, chunked indexed sequence with concurrently mutable chunks.
-  * `Vector` elements correspond to a chunk of constant length.
   *
-  * @param chunkSize the size of chunks that sit at `Vector` elements
+  * @param vector sequence of chunks  of constant length
+  * @param chunkSize the size of chunks that sit at `vector`'s elements
   * @tparam A type of elements
   * @tparam C type of chunk-level data
   */
-final protected[concurrent] class VectorArraySeq[A <: AnyRef, C] private (chunkSize: Size, vector: Vector[Chunk[A, C]]):
+final protected[concurrent] class VectorArraySeq[A, C] private (vector: Vector[Chunk[A, C]])(implicit
+    val chunkSize: Size
+):
+
+  private type This = VectorArraySeq[A, C]
+
+  private def appended(chunk: Chunk[A, C]): This =
+    new VectorArraySeq[A, C](vector :+ chunk)
 
   def chunkElem(i: Index): (C, A) =
     val c = chunk(i)
@@ -26,23 +43,53 @@ final protected[concurrent] class VectorArraySeq[A <: AnyRef, C] private (chunkS
 
   def chunk(i: Index): Chunk[A, C] = vector(i / chunkSize)
 
-  /** Adds `elem` to this collection in a thread-safe way. */
-  def addOne(elem: A)(implicit chunkPrefix: C): Index = ???
+  def size: Size =
+    val chunkCount = vector.size
+    if chunkCount > 0 then (chunkCount - 1) * chunkSize + vector.last.used
+    else 0
 
-  /** Alias for `addOne`. */
-  inline def +=(elem: A)(implicit chunkPrefix: C): Index = addOne(elem)
+  def lastIndex: Index = size - 1
+
+  /** Adds `elem` to `this` in a thread-safe way or creates new instance with the elements of `this` plus `elem`. */
+  def appended(elem: A)(implicit chunkPrefix: C): This =
+    vector.lastOption match
+      case Some(last) if last.used < chunkSize && last.prefix == chunkPrefix =>
+        // TODO lock
+        last.array(lastIndex + 1) = elem
+        last.used += 1
+        this
+      case _ =>
+        appended(Chunk(chunkPrefix, elem))
+
+  /** Alias for `appended`. */
+  inline def :+(elem: A)(implicit chunkPrefix: C): This = appended(elem)
 
   /** Computes a new collection with the contents of this collection but the `chunk` at `c`. */
-  def updated(c: Index, newChunk: (C, Array[A])): VectorArraySeq[A, C] = ???
+  def updated(c: Index, newChunk: (C, Array[A])): VectorArraySeq[A, C] =
+    ???
+
+  def iterator: Iterator[A] =
+    vector.iterator.flatMap { case Chunk(_, used, arr) =>
+      arr.iterator.slice(0, used)
+    }
+
+  private[concurrent] def trace: String =
+    val v = vector.iterator
+      .map { case Chunk(prefix, used, arr) =>
+        s"""prefix: $prefix, used: $used, {${arr.map(elem => Option(elem).fold("null")(_.toString)) mkString ", "}}"""
+      }
+      .mkString("  ", "\n  ", "")
+    s"""chunkSize: $chunkSize, size: $size, chunks:
+       |$v""".stripMargin
 
 private[concurrent] object VectorArraySeq:
 
-  def empty[A <: AnyRef, C](sizeHint: Int, writeIntensive: Boolean = false): VectorArraySeq[A, C] =
-    new VectorArraySeq[A, C](chunkSize(sizeHint, writeIntensive), Vector.empty[Chunk[A, C]])
+  def empty[A, C](sizeHint: Size, writeIntensive: Boolean = false): VectorArraySeq[A, C] =
+    new VectorArraySeq[A, C](Vector.empty[Chunk[A, C]])(chunkSize(sizeHint, writeIntensive))
 
   private[concurrent] val MinChunkSize = 16
 
-  private[concurrent] def chunkSize(sizeHint: Int, heavyUtilization: Boolean): Int =
+  private[concurrent] def chunkSize(sizeHint: Size, heavyUtilization: Boolean): Int =
     import Integer.{highestOneBit, numberOfTrailingZeros}
 
     def approxSqrt(i: Int): Int        = 1 << (numberOfTrailingZeros(highestOneBit(i)) >> 1)
--- concurrent/src/test/scala/scalax/collection/concurrent/VectorArraySeqSpec.scala
+++ concurrent/src/test/scala/scalax/collection/concurrent/VectorArraySeqSpec.scala
@@ -1,12 +1,17 @@
 package scalax.collection.concurrent
 
+import scala.util.chaining._
+
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.refspec.RefSpec
 
 class VectorArraySeqSpec extends RefSpec with Matchers:
   import VectorArraySeq._
 
-  def `chunk size based on hints`(): Unit =
+  private def tinySeq[A, C] =
+    VectorArraySeq.empty[A, C](2) tap (_.chunkSize shouldBe MinChunkSize)
+
+  def `'chunkSize' based on hints`(): Unit =
     List(
       10        -> (MinChunkSize, MinChunkSize),
       300       -> (MinChunkSize, 32),
@@ -20,3 +25,14 @@ class VectorArraySeqSpec extends RefSpec with Matchers:
       normal should (be >= expectedMinChunkSize and be <= expectedMaxChunkSize)
       if normal > MinChunkSize then chunkSize(sizeHint, heavyUtilization = true) should (be < normal)
     }
+
+  def `'appended' within chunk, single-threaded`(): Unit =
+    given prefix: Int = 0
+    val elem          = 0
+    var seq           = tinySeq[elem.type, Int]
+    (1 to seq.chunkSize) foreach { i =>
+      seq = seq :+ elem
+      seq.size shouldBe i
+      seq.elem(i - 1) shouldBe elem
+    }
+    seq.iterator.length shouldBe seq.chunkSize
