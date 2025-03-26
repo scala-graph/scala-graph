afb3e28be8c4d44f17b40206e5d91e2b9fb842a3
Peter Empen <peter.empen@commercetools.com>
2025-03-26 10:20:17 +0100

    bump version

--- project/Version.scala
+++ project/Version.scala
@@ -1,5 +1,5 @@
 object Version {
-  val compiler_2_13 = "2.13.15"
+  val compiler_2_13 = "2.13.16"
   val compiler_3    = "3.5.1"
 
   val compiler_3_fallback = "3.3.0"
@@ -11,8 +11,8 @@ object Version {
   private val minor               = 0
   private def version(patch: Int) = s"$major.$minor.$patch$snapshot"
 
-  val highest = version(2)
-  val core    = version(2)
+  val highest = version(3)
+  val core    = version(3)
   val dot     = version(0)
   val json    = version(0)
 }
